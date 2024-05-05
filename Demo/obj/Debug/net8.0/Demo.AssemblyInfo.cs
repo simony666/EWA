592a8d9b7010a4d81dfaf203829b1fe927bf9b86
Zzz-anonymous <firsttry2468@gmail.com>
2024-05-05 09:52:29 +0800

    add tutors, students, classes, subjects table

--- Demo/obj/Debug/net8.0/Demo.AssemblyInfo.cs
+++ Demo/obj/Debug/net8.0/Demo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Demo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21948226c76d0e2d5e54c35093ef2abaff5f908a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Demo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Demo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
