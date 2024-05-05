b069423fe6bbe2d55ca6199cb4958740cb3e9f21
Simon Yong <51854826+simony666@users.noreply.github.com>
2024-05-05 16:50:34 +0800

    Squashed commit of the following:
    commit c66c7c4203b3189f57b07d55973425f367572368
    Author: Simon Yong <51854826+simony666@users.noreply.github.com>
    Date:   Sun May 5 14:22:04 2024 +0800
    
        Update .gitignore
    
    commit 5564c936a146fa2a69ebef71b38445036b83ccd8
    Author: Simon Yong <51854826+simony666@users.noreply.github.com>
    Date:   Sun May 5 13:56:43 2024 +0800
    
        Update .gitignore
    
    commit 7d13d63d05504d7555f62b22ac35b1108b86db8f
    Author: Simon Yong <51854826+simony666@users.noreply.github.com>
    Date:   Sun May 5 11:51:13 2024 +0800
    
        Update .gitignore
    
    commit ed68ec99f1ec3c78d3317ab2c6039f53e3b5e090
    Author: Simon Yong <51854826+simony666@users.noreply.github.com>
    Date:   Sun May 5 11:48:38 2024 +0800
    
        Delete Useless Item
    
    commit 212861e2d8a69751e2646295de1f54e8f1f32438
    Author: Simon Yong <51854826+simony666@users.noreply.github.com>
    Date:   Sun May 5 11:47:18 2024 +0800
    
        Update .gitignore
    
    commit bd4f424931c15e81c5d887e97ac8f84f03a48fe2
    Author: Zzz-anonymous <firsttry2468@gmail.com>
    Date:   Sun May 5 11:45:02 2024 +0800
    
        change path in ViewStart
    
    commit a4f85b06b8c06faca84cb76306bc0f8eee83a8cd
    Author: Zzz-anonymous <firsttry2468@gmail.com>
    Date:   Sun May 5 11:38:49 2024 +0800
    
        modify css, js, stafflayout
    
    commit 0e8ce94e558467bd0a37c93da82febe7ee6a705d
    Author: Zzz-anonymous <firsttry2468@gmail.com>
    Date:   Sun May 5 11:37:27 2024 +0800
    
        modify css, js, stafflayout
    
    commit 592a8d9b7010a4d81dfaf203829b1fe927bf9b86
    Author: Zzz-anonymous <firsttry2468@gmail.com>
    Date:   Sun May 5 09:52:29 2024 +0800
    
        add tutors, students, classes, subjects table

--- Demo/Models/DB.cs
+++ Demo/Models/DB.cs
@@ -11,6 +11,10 @@ public class DB : DbContext
     // DB Sets
     public DbSet<User> Users { get; set; }
     public DbSet<Admin> Admins { get; set; }
+    public DbSet<Subjects> Subjects { get; set; }
+    public DbSet<Tutors> Tutors { get; set; }
+    public DbSet<Students> Students { get; set; }
+    public DbSet<Classes> Classes { get; set; }
 }
 
 // Entity Classes
@@ -32,7 +36,81 @@ public class User
     public string Role => GetType().Name;
 }
 
+
 public class Admin : User
 {
+
+}
+
+public class Subjects
+{
+    [Key, MaxLength(100)]
+    public string Id { get; set; }
+    [MaxLength(100)]
+    public string Name { get; set; }
+    [Precision(6, 2)]
+    public decimal Fees { get; set; }
+    public DateTime StartTime { get; set; }
+    public DateTime EndTime { get; set; }
+    
+    public int Duration { get; set; }
+
+    [MaxLength(100)]
+    public string DayOfWeek { get; set; }
+
+    // FK
+    public string TutorsId { get; set; }
+    public string StudentsId { get; set; }
+
+    // Navigation
+    public List<Tutors> TutorSubjects { get; set; } = new();
+    public List<Students> StudentSubjects { get; set; } = new();
+}
+
+public class Tutors
+{
+    [Key, MaxLength(100)]
+    public string Id { get; set; }
+    [MaxLength(100)]
+    public string Name { get; set; }
+    [MaxLength(100)]
+    public string Gender { get; set; }
+    [MaxLength(100)]
+    public string PhotoURL { get; set; }
+
+    public int Age { get; set; }
+
+    // FK
+    public string ClassesId { get; set; }
+}
+
+public class Students
+{
+    [Key, MaxLength(100)]
+    public string Id { get; set; }
+    [MaxLength(100)]
+    public string Name { get; set; }
+    [MaxLength(100)]
+    public string Gender { get; set; }
+    [MaxLength(100)]
+    public string PhotoURL { get; set; }
+
+    // FK
+    public string SubjectsId { get; set; }
+    public string ClassesId { get; set; }
+
+    // Navigation
+    public Tutors Tutors { get; set; }
+    public Classes Classes { get; set; }
+}
+
+public class Classes
+{
+    [Key, MaxLength(100)]
+    public string Id { get; set; }
+    [MaxLength(100)]
+    public string Name { get; set; }
+    
+    public int Capacity { get; set; }
 
 }
