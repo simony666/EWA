92b279c62851ff93d14b303361301a8c9ea33788
Simon Yong <51854826+simony666@users.noreply.github.com>
2024-05-16 00:18:13 +0800

    Attendance Controller

--- Demo/Controllers/AttendanceController.cs
+++ Demo/Controllers/AttendanceController.cs
@@ -1,6 +1,24 @@
-﻿namespace Demo.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Demo.Controllers
 {
-    public class AttendanceController
+    public class AttendanceController : Controller
     {
+        private readonly DB db;
+        private readonly IWebHostEnvironment en;
+        private readonly Helper hp;
+
+        public AttendanceController(DB db, IWebHostEnvironment en, Helper hp)
+        {
+            this.db = db;
+            this.en = en;
+            this.hp = hp;
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.Classes = db.Classes;
+            return View();
+        }
     }
 }
